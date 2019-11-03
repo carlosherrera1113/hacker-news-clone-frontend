64b263ee559d3b8b4ed571d22fd959f767f81d00
Carlos Herrera <carlitosherrera1113@gmail.com>
2019-11-03 16:16:26 -0600

    more styled component changes in createLink.tsx

--- src/components/createLink.tsx
+++ src/components/createLink.tsx
@@ -9,15 +9,45 @@ font-family: sans-serif;
 font-size: 1.3rem;
 color: white;
 background-color: darkorange;
+justify-content: center;
 text-align: center;
 padding: 0.3rem 1rem 0.3rem;
 cursor: pointer;
 border-style: none;
-border-radius: 100px;
-transition: all 0.2s;
+border-radius: 6.25rem;
+transition: all 0.2s ease-out;
 &:hover {
     transform: translateY(-1px) scale(1.01);
     background: #ffaa00;
+    box-shadow: 0rem 0.75rem 2.5rem rgba(255, 170, 0, 0.25);
+}
+`;
+
+const StyledDiv = styled.div`
+width: 50%;
+display: flex;
+position: center;
+margin: 3rem;
+flex-direction: column;
+justify-content: center;
+`;
+
+const StyledInput = styled.input`
+padding: 1.2rem 2.5rem;
+background-image: white;
+font-family: inherit;
+font-size: inherit;
+border-radius: 6.25rem;
+border: 0.1rem solid rgba(255, 170, 0, 0.25);
+margin-top: 1.5rem;
+background-color: transparent;
+transition: all 300ms;
+color: gray;
+&::placeholder {
+color: gray;
+}
+&:hover {
+    transform: translateY(-0.1rem) scale(1.01);
     box-shadow: 0rem 0.75rem 2.5rem rgba(255, 170, 0, 0.25);
 }
 `;
@@ -43,20 +73,20 @@ const CreateLink: React.FC = () => {
 
   return (
     <div>
-      <div>
-        <input
+      <StyledDiv>
+        <StyledInput
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           type="text"
           placeholder="A description for the link."
         />
-        <input
+        <StyledInput
           value={url}
           onChange={(e) => setUrl(e.target.value)}
           type="text"
           placeholder="The URL for the link."
         />
-      </div>
+      </StyledDiv>
       <StyledButton type="button" onClick={() => postMutation(POST_MUTATION)}>Submit Link</StyledButton>
     </div>
   );
