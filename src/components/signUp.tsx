1b6167b69ed89150693a9e115c345b12e4663c3b
Carlos Herrera <carlitosherrera1113@gmail.com>
2019-12-05 14:04:04 -0600

    created signUp component

--- src/components/signUp.tsx
+++ src/components/signUp.tsx
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import { gql } from 'apollo-boost';
+import { useSignUpMutation } from '../generated/graphql';
+
+const SIGNUP_MUTATION = gql`
+  mutation SignUp($email: String!, $password: String!, $name: String!) {
+    signUp(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
 
 const SignUp: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const history = useHistory();
+  const [signUp] = useSignUpMutation(SIGNUP_MUTATION);
+
   return (
-    <div>SignUp</div>
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const response = await signUp({
+          variables: {
+            email,
+            name,
+            password,
+          },
+        });
+        console.log(response);
+        history.push('/');
+      }}
+    >
+      <div>
+        <input
+          value={email}
+          placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <input
+          value={name}
+          placeholder="Username"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
