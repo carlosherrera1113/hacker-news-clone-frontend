b2c9a737091fe8802ab4d2d6193323864b9d323f
Carlos Herrera <carlitosherrera1113@gmail.com>
2020-02-07 14:05:44 -0600

    small changes to useEffect hook in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Routes from './components/routes';
 import { setAccessToken } from './utils/accessToken';
 
 const App: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-
+  // This allows the global variable 'accessToken' to be refreshed when the user refreshes
+  // the page after login
   useEffect(() => {
     fetch('http://localhost:4000/refresh_token', {
       method: 'POST',
@@ -12,13 +12,9 @@ const App: React.FC = () => {
     }).then(async (x) => {
       const { accessToken } = await x.json();
       setAccessToken(accessToken);
-      setLoading(false);
     });
   }, []);
 
-  if (loading) {
-    return <div>token</div>;
-  }
   return (<Routes />);
 };
 
--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import CreateLink from './createLink';
-import LinkContainer from './linkContainer';
 import NavBar from './navigation/navBar';
 import SignUp from './auth/signUp';
 import Login from './auth/login';
+import CreateLink from './createLink';
+import LinkContainer from './linkContainer';
 
-const Routes: React.FC = () => {
-  return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={LinkContainer} />
-        <Route exact path="/create" component={CreateLink} />
-        <Route exact path="/signUp" component={SignUp} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Router>
-  );
+const Routes = () => {
+    return (
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={LinkContainer} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/signUp" component={SignUp} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+    );
 };
 
 export default Routes;
